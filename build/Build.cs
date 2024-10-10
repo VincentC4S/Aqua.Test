611fe1a164c33c5f0cb347ddbe8be6747d284511
Vincent Broeke <vincent@itransact.nl>
2024-10-10 18:12:48 +0200

    Initial commit

---
+++ build/Build.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities.Collections;
+using Serilog;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+
+[GitHubActions("workflow", GitHubActionsImage.WindowsServer2019, AutoGenerate = true, On = [GitHubActionsTrigger.Push])]
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+    GitHubActions GitHubActions => GitHubActions.Instance;
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+        });
+
+    Target Print => _ => _
+        .After(Compile)
+        .Executes(() =>
+        {
+            Console.WriteLine("Branch = {Branch}", GitHubActions.Ref);
+            Console.WriteLine("Commit = {Commit}", GitHubActions.Sha);
+            Log.Information("Branch = {Branch}", GitHubActions.Ref);
+            Log.Information("Commit = {Commit}", GitHubActions.Sha);
+        });
+}
---
+++ run.ps1
@@ -0,0 +1,1 @@
+& dotnet watch run --project ./src/Aqua.TestProject/Aqua.TestProject.csproj
